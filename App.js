7b8036fe341ae9970f44a3859fa822fbdbb58085
luis10g <joicefernandasouzaoliveira@gmail.com>
2021-11-25 17:50:26 +0000

    2Aplicação

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
+function enviar(){
+  alert('Você clicou no botão!');
+}
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titulo}>Segundo Aplicativo</Text>
+      <TextInput style={styles.campo}></TextInput>
+      <TouchableOpacity style={styles.botao} onPress={enviar}>
+        <Text style={styles.textobotao}>Mostrar</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +22,36 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //alignItems: 'center',
+    //justifyContent: 'center',
   },
+titulo:{
+  fontSize: 30,
+  textAlign: 'center',
+  marginTop: 15,
+  color: '#800000'
+
+},
+
+campo:{
+  backgroundColor: '#FFA07A',
+  fontSize: 20,
+  margin: 15,
+  padding: 10,
+  color: '#FFF',
+  borderRadius: 20
+},
+
+botao:{
+  backgroundColor: '#800000',
+  margin: 15,
+  alignItems: 'center',
+  padding: 10,
+  borderRadius: 20
+},
+
+textobotao:{
+  color: 'FFF',
+  fontSize: 25
+}
 });
